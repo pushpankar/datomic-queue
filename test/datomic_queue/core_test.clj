3bf74e229f8cf30cd27a3223a18ffc3bf3ee632b
Pushpankar <pushpankark@gmail.com>
2023-03-31 12:27:44 +0100

    Use fixture

--- test/datomic_queue/core_test.clj
+++ test/datomic_queue/core_test.clj
@@ -3,14 +3,7 @@
             [datomic-queue.core :as q]
             [datomic.client.api :as d]))
 
-(def client (d/client {:server-type :dev-local
-                       :storage-dir :mem
-                       :system "ci"}))
-
-(d/create-database client {:db-name "dm-test-queue"})
-(def conn (d/connect client {:db-name "dm-test-queue"}))
-(q/init-db conn)
-
+;; Test utils
 (defmacro is-not [form]
   `(is (not ~form)))
 
@@ -21,37 +14,49 @@
   `(= (count ~coll) ~value))
 
 
+(def ^:dynamic *conn* nil)
+
+(defn setup [f]
+  (let [client (d/client {:server-type :dev-local
+                          :storage-dir :mem
+                          :system "ci"})
+        _ (d/create-database client {:db-name "dm-test-queue"})
+        conn (d/connect client {:db-name "dm-test-queue"})
+        _ (q/init-db conn)]
+    (binding [*conn* conn]
+      (f))))
+
 
 (deftest push-peek-test
   (testing "Peek on empty queue"
-    (let [queue (q/create-dbqueue conn)]
+    (let [queue (q/create-dbqueue *conn*)]
       (is (nil? (q/dpeek queue)))))
   (testing "Peek on non-empty queue"
-    (let [queue (q/create-dbqueue conn)
+    (let [queue (q/create-dbqueue *conn*)
           _ (q/dpush queue "1")]
       (is-not (nil? (q/dpeek queue))))))
 
 (deftest push-peek-last-test
   (testing "Peek last on empty queue"
-    (let [queue (q/create-dbqueue conn)]
+    (let [queue (q/create-dbqueue *conn*)]
       (is (nil? (q/dpeek-last queue)))))
   (testing "Peek last on queue with one node"
-    (let [queue (q/create-dbqueue conn)
+    (let [queue (q/create-dbqueue *conn*)
           _ (q/dpush queue "1")]
       (is-not (nil? (q/dpeek-last queue))))))
 
 (deftest push-pop-test
   (testing "Pop empty queue"
-    (let [queue (q/create-dbqueue conn)]
+    (let [queue (q/create-dbqueue *conn*)]
       (is (nil? (q/dpop queue)))))
   (testing "Pop non-empty queue"
-    (let [queue (q/create-dbqueue conn)
+    (let [queue (q/create-dbqueue *conn*)
           _ (q/dpush queue "1")]
       (is-not (nil? (q/dpeek queue)))
       (is-not (nil? (q/dpop queue)))
       (is (nil? (q/dpeek queue)))))
   (testing "Drain and fill the queue"
-    (let [queue (q/create-dbqueue conn)
+    (let [queue (q/create-dbqueue *conn*)
           _ (q/dpush queue "1")
           _ (q/dpush queue "2")
           _ (q/dpush queue "3")]
@@ -61,4 +66,5 @@
       (q/dpush queue "1")
       (is-not (nil? (q/dpeek queue))))))
 
+(use-fixtures :once setup)
 (run-all-tests)
